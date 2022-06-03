a4e4f255e7861dcd8f2094710229fa4c285250a3
JubayerDev <ajashes858@gmail.com>
2022-06-04 02:34:26 +0600

    hero page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,3 @@
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("daisyui")],
-//   daisyui: {
-//     themes: [
-//       {
-//         mytheme: {
-//           primary: "#0FCFEC",
-//           secondary: "#19D3AE",
-//           accent: "#3A4256",
-//           neutral: "#3d4451",
-//           "base-100": "#ffffff",
-//         },
-//       },
-//       "dark",
-//       "cupcake",
-//     ],
-//     styled: true,
-//     base: false,
-//     utils: true,
-//     logs: true,
-//     rtl: false,
-//     prefix: "",
-//     darkTheme: "dark",
-//   },
-// }
-
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
