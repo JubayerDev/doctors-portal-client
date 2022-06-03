b2466f3cdbd1761f3880976a8f27be25eb885347
JubayerDev <ajashes858@gmail.com>
2022-06-04 01:48:47 +0600

    navbar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+//   daisyui: {
+//     themes: [
+//       {
+//         mytheme: {
+//           primary: "#0FCFEC",
+//           secondary: "#19D3AE",
+//           accent: "#3A4256",
+//           neutral: "#3d4451",
+//           "base-100": "#ffffff",
+//         },
+//       },
+//       "dark",
+//       "cupcake",
+//     ],
+//     styled: true,
+//     base: false,
+//     utils: true,
+//     logs: true,
+//     rtl: false,
+//     prefix: "",
+//     darkTheme: "dark",
+//   },
+// }
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
@@ -19,7 +49,6 @@ module.exports = {
       "dark",
       "cupcake",
     ],
-    base: false,
   },
   plugins: [require("daisyui")],
 }
