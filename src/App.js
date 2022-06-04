76f7274b704be8a4e1701a7e50ec72f8e1155443
JubayerDev <ajashes858@gmail.com>
2022-06-04 15:23:05 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Footer from './Pages/Footer/Footer';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
@@ -14,6 +15,7 @@ function App() {
         <Route path='/about' element={ <About />}/>
         <Route path='/login' element={ <Login />}/>
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -4,25 +4,33 @@ import PrimaryBtn from '../Shared/PrimaryBtn'
 
 const Contact = () => {
     return (
-        <div style={{ background: `url('${appointment}')` }} className=' lg:h-screen'>
-            <div className='text-center pt-16 pb-10 mb-10'>
-                <h3 className='text-xl font-bold text-secondary'>Contact Us</h3>
-                <h2 className='text-3xl font-semibold text-white'>Stay Connected With Us</h2>
-            </div>
-            <div className='w-[33%] mx-auto'>
-                <div className="form-control">
-                    <label className="input-group mb-5">
-                        <input type="text" placeholder="Email Address" className="text-xl w-[450px] input input-bordered" />
-                    </label>
-                    <label className="input-group mb-5">
-                        <input type="text" placeholder="Subject" className="text-xl w-[450px] input input-bordered" />
-                    </label>
-                    <label className="input-group mb-5">
-                        <textarea type="text" placeholder="Your Message" className="text-xl w-[450px] input input-bordered" rows='5' cols='30'/>
-                    </label>
-                    <PrimaryBtn>Submit</PrimaryBtn>
-                </div>
-            </div>
+        <div style={{
+            background:`url(${appointment})`
+        }} className='bg-primary px-10 py-14 '>
+          <div className='text-center pb-14 text-white'>
+            <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary'>
+              Contact Us
+            </p>
+            <h1 className='text-4xl font-semibold'>Stay connected with us</h1>
+          </div>
+          <div className='grid grid-cols-1 justify-items-center gap-5'>
+            <input
+              type='text'
+              placeholder='Email Address'
+              className='input w-full max-w-md'
+            />
+            <input
+              type='text'
+              placeholder='Subject'
+              className='input w-full max-w-md'
+            />
+            <textarea
+              className='textarea w-full max-w-md'
+              placeholder='Your message'
+              rows={6}
+            ></textarea>
+            <PrimaryBtn>Submit</PrimaryBtn>
+          </div>
         </div>
     );
 };
