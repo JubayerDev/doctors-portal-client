ed2f6d577dc48b0420662b22b265aa0f2acda1b0
JubayerDev <ajashes858@gmail.com>
2022-06-04 12:26:18 +0600

    dental care added

--- src/Pages/Home/DentalCare.js
+++ src/Pages/Home/DentalCare.js
@@ -7,7 +7,7 @@ const DentalCare = () => {
         <div className="hero min-h-screen">
             <div className="hero-content flex flex-col lg:flex-row items-center justify-between">
                 <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" alt='treatment'/>
-                <div className='w-2/4'>
+                <div className='w-2/4 pr-12'>
                     <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                     <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <PrimaryBtn />
