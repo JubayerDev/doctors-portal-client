0f507c634c4d8ba8c4f409a234f4470d73b97463
JubayerDev <ajashes858@gmail.com>
2022-06-10 01:54:25 +0600

    google sign-in implementation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
-        <div>
-            Login
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-3xl font-semibold">Login</h2>
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline" onClick={()=>signInWithGoogle()}>Continue With Google</button>
+                </div>
+            </div>
         </div>
     );
 };
