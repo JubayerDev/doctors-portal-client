7bbf892ca80a70b7f6c02e9a4219794d2e20e754
JubayerDev <ajashes858@gmail.com>
2022-06-10 02:44:50 +0600

    sign-in form and validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,67 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
 import auth from '../../firebase.init';
-import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center text-3xl font-semibold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Your Email" className="input input-bordered w-full max-w-xs" {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: 'Email is Required'
+                                },
+                                pattern: {
+                                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                    message: 'Provide a Valid Email'
+                                }
+                            })} />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className="label-text-alt text-red-700">{ errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-700">{ errors.email.message}</span>}
+                                
+                            </label>
+                        </div>
+
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="Your Password" className="input input-bordered w-full max-w-xs" {...register("password", {
+                                required: {
+                                    value: true,
+                                    message: 'Password is Required'
+                                },
+                                minLength: {
+                                    value: 8,
+                                    message: 'Must be 8 characters or longer'
+                                }
+                            })} />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-700">{ errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-700">{ errors.password.message}</span>}
+                                
+                            </label>
+                        </div>
+                        <input className='btn w-full max-w-xs text-white text-xs tracking-widest font-semibold' type="submit" value={'Login'} />
+                    </form>
                     <div className="divider">OR</div>
-                    <button className="btn btn-outline" onClick={()=>signInWithGoogle()}>Continue With Google</button>
+                    <button className="btn btn-outline" onClick={() => signInWithGoogle()}>Continue With Google</button>
                 </div>
             </div>
         </div>
